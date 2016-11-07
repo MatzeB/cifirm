5949b362df7a2d91a91bd2690618b5bec3856df1
Matthias Braun <matze@braunis.de>
2016-11-06 22:06:06 -0800

    update

--- firm.sh
+++ firm.sh
@@ -2,6 +2,7 @@
 set -eu
 
 . common
+CONFIGDESC="$(git describe --abbrev=40 --always --dirty --match '')"
 
 WORKDIR="$1"
 REVISION="$2"
@@ -20,9 +21,10 @@ make DESTDIR="$WORKDIR/libfirm_install" PREFIX="" install
 cd "$WORKDIR/libfirm_install"
 git init .
 git add -Af .
-git commit -m "build/$DESC from libfirm $DESC"
-git tag "build/$DESC"
+TAGNAME="build/${DESC}_${CONFIGDESC}"
+git commit -m "$TAGNAME from libfirm $DESC ciconfig $CONFIGDESC"
+git tag "$TAGNAME"
 git remote add dest "$DEST"
-git push -f dest "build/$DESC"
+git push -f dest "$TAGNAME"
 #rm -rf "$WORKDIR/libfirm"
 #rm -rf "$WORKDIR/libfirm_install"

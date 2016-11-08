4c131668d6e9c4f5293b756a0765f08160d99d48
Matthias Braun <matze@braunis.de>
2016-11-07 22:47:59 -0800

    rename, update

--- test_libfirm.sh
+++ test_libfirm.sh
@@ -1,3 +1,3 @@
 . common
 LIBFIRM_REV="$(git --git-dir ${LIBFIRM_REPO} rev-parse master)"
-./firm.sh /tmp/testbuild ${LIBFIRM_REV} /tmp/artifacts
+./libfirm.sh /tmp/testbuild ${LIBFIRM_REV} /tmp/artifacts
